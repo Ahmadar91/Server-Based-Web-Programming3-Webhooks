5deb7ca451143dc1faace833b10efa6a3351bc28
Ahamd <aa224fn@student.lnu.se>
2020-01-06 17:02:24 +0100

    port

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express()
 
 const GithubHook = require('express-github-webhook')
 const hook = GithubHook({ path: '/webhook', secret: process.env.Token })
-const PORT = 3000
+
+const PORT = process.env.port || 3000
+
 const server = http.createServer(app).listen(PORT, function () {
   console.log('Started: listing on port', PORT)
 })
