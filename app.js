90f59c432ed3e395e6574a658a41aeef6e442604
Ahamd <aa224fn@student.lnu.se>
2020-01-06 16:17:39 +0100

    json

--- app.js
+++ app.js
@@ -33,22 +33,22 @@ app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 hook.on('issues', function (repo, data) {
-  // console.log(data)
-  const data1 = JSON.parse(data.payload)
+  console.log(data)
+  // const data1 = JSON.parse(data.payload)
   // console.log('data1')
   // console.log(data1)
-  console.log(data1.action)
-  console.log(data1.issue.title)
-  console.log(data1.issue.comments)
+  console.log(data.action)
+  console.log(data.issue.title)
+  console.log(data.issue.comments)
 })
 hook.on('issue_comment', function (repo, data) {
-  const data1 = JSON.parse(data.payload)
+  // const data1 = JSON.parse(data.payload)
   console.log('----Comments ----')
 
-  console.log(data1.issue.comments)
-  console.log(data1.issue.number)
-  console.log(data1.issue.title)
-  console.log(data1.comment.body)
+  console.log(data.issue.comments)
+  console.log(data.issue.number)
+  console.log(data.issue.title)
+  console.log(data.comment.body)
 })
 
 hook.on('error', function (err, req, res) {
